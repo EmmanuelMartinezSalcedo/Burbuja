9300b8c0b1319bbfc14327ee70053276e1136f2d
milksake <caceresmariana443@gmail.com>
2025-01-26 09:08:49 -0500

    shader + bullet collision

---
+++ Scripts/shader.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@export var camera : Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position = camera.get_screen_center_position()
